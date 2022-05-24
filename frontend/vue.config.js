a72c103c4dd596e2197e0834613a8885dbb68e79
hanebarla <barla.wings@gmail.com>
2022-05-25 00:27:12 +0900

    delete multiple public path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   publicPath: '/homepage',
   outputDir: '../docs',
-  assetsDir: '../',
-  publicPath: '../',
+  assetsDir: './',
   transpileDependencies: [
     'vuetify'
   ]
